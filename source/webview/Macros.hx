adfb4b0ccc8345d8fad23a762f9617db2b31b806
SanicBTW <sansunder718jqda@gmail.com>
2024-05-29 09:58:04 +0200

    Fixed macro not copying DLLs properly.

--- source/webview/Macros.hx
+++ source/webview/Macros.hx
@@ -1,11 +1,11 @@
 package webview;
 
-import haxe.macro.Compiler;
+import sys.io.File;
 import haxe.io.Path;
 import sys.FileSystem;
-import sys.io.File;
 import haxe.macro.Expr;
 import haxe.macro.Context;
+import haxe.macro.Compiler;
 
 using haxe.macro.PositionTools;
 
@@ -14,10 +14,14 @@ class Macros
     // This macro will only run on Windows targets to copy over the EmbeddedBrowserWebView DLL
     macro public static function copyDLLs():Array<Field>
     {
+        var libDir:String = Path.directory(FileSystem.fullPath(Context.currentPos().getInfos().file)); // HxWebView/source/webview
+        libDir = Path.directory(libDir); // HxWebView/source
+        libDir = Path.directory(libDir); // HxWebView
+
         Context.onAfterGenerate(() ->
         {
             var arch:String = #if HXCPP_M64 "x64" #else "x86" #end;
-            var dllsPath:String = Path.join([Sys.getCwd(), "internal", "windows", "EBWebView", arch]);
+            var dllsPath:String = Path.join([libDir, "internal", "windows", "EBWebView", arch]);
             if (FileSystem.exists(dllsPath))
             {
                 dllsPath = Path.join([dllsPath, "EmbeddedBrowserWebView.dll"]);
